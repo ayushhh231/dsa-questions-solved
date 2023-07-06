aa3b945119375bd366bbc45ddcc9b3b2a214420a
Ayushmaan Sharma <106451252+ayushhh231@users.noreply.github.com>
2023-07-06 14:30:38 +0530

    Add files via upload

---
+++ leftrightsum.java
@@ -0,0 +1,23 @@
+// leetcode - 2574 question
+
+public class leftrightsum {
+     public int[] leftRightDifference(int[] nums) {
+        int n = 0;
+        int l = nums.length;
+        int[] ans = new int[l];
+
+        for (int i = 0; i < l; i++) {
+            ans[i] = n;
+            n += nums[i];
+        }
+
+        n = 0;
+
+        for (int i = l-1; i >= 0; i--) {
+            ans[i] = Math.abs(ans[i] - n);
+            n += nums[i];
+        }
+
+        return ans;
+    }
+}
